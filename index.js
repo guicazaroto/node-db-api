7c346be500eb93f7be293cf97e3e682b8eee7e52
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2023-07-02 18:34:48 -0300

    Revert "Project new version"

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 import server from './server.js'
 
-// Starting server
-const port = process.env.PORT || 3000;
-server.listen(port, (err) => {
-    if (!err)
-        console.log(`Server is Successfully Running, and App is listening on port ${port}`)
-    else
-        console.log("Error occurred, server can't start", err);
-}
-);
+server.listen(process.env.PORT || 3000)
