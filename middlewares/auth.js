c5c08357f85b3f3ffd2baefc03888f4fcfca5bab
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2023-07-02 19:02:19 -0300

    add auth base

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,36 @@
+import jwt from 'jsonwebtoken'
+
 export default function auth (req, res) {
-  const { authorization } = req.headers
-  if (!authorization) {
-    return res.status(401).json({ message: 'Não autorizado' })
-  }
-  const token = authorization.replace('Bearer', '').trim()
-  if (!token) {
-    return res.status(401).json({ message: 'Não autorizado' })
-  }
 
-  // verifica se o token é valido
-
-  return res.status(200).json({ message: 'Autorizado' })
 }
+
+// const jwt = require('jsonwebtoken')
+// apiRouter.post(endpoint + 'seguranca/login', (req, res) => {
+  // knex
+  // .select('*').from('usuario').where( { login: req.body.login })
+  // .then( usuarios => {
+  // if(usuarios.length){
+  // let usuario = usuarios[0]
+  // let checkSenha = bcrypt.compareSync (req.body.senha, usuario.senha)
+  // if (checkSenha) {
+  // var tokenJWT = jwt.sign({ id: usuario.id },
+  // process.env.SECRET_KEY, {
+  // expiresIn: 3600
+  // })
+  // res.status(200).json ({
+  // id: usuario.id,
+  // login: usuario.login,
+  // nome: usuario.nome,
+  // roles: usuario.roles,
+  // token: tokenJWT
+  // })
+  // return
+  // }
+// }
+    // res.status(200).json({ message: 'Login ou senha incorretos' })
+  // })
+// .catch (err => {
+  // res.status(500).json({
+   // message: 'Erro ao verificar login - ' + err.message })
+  // })
+// })
