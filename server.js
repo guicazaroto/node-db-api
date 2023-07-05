15b5bbc6e476ea49a2ef6666fdfded59909b6191
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2023-07-04 21:19:37 -0300

    update routes

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ import routes from './routes.js'
 import privateRoutes from './private-routes.js'
 import { config } from 'dotenv';
 import {auth, isAdmin} from './middlewares/auth.js';
-
+import cors from 'cors'
 
 config()
 
 
 const app = express()
 
+app.use(cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.json())
+
+app.get('/', (_, res) => { res.render('index'); })
 app.use('/v1', privateRoutes)
 app.use('/v2/seguranca', routes)
 app.use('/v2', auth, isAdmin, privateRoutes)
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
 
 export default app
